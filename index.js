b2fa93a7fef9d80b664ae6c9aa2e17bde961f504
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-02-23 15:33:34 -0800

    web smit

---
+++ index.js
@@ -0,0 +1,8 @@
+var sirahmedpic = document.getElementById("sirahmedpic")
+
+
+setInterval (() =>{
+if (sirahmedpic.src.includes("sirahmed.jfif")){
+    sirahmedpic.src = "./sir zia.jfif"
+}
+},2000 )
