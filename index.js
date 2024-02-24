026ba793dbb0fd20cb068c704aba4099e5264c3d
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-02-24 14:36:08 -0800

    DONE

--- index.js
+++
@@ -1,8 +0,0 @@
-var sirahmedpic = document.getElementById("sirahmedpic")
-
-
-setInterval (() =>{
-if (sirahmedpic.src.includes("sirahmed.jfif")){
-    sirahmedpic.src = "./sir zia.jfif"
-}
-},2000 )
